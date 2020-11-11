48237596584e3a070ce5420c043e311f9418c1df
sevket <sevket.buyukdemir@gmail.com>
2020-11-11 05:13:04 +0300

    Foreign Exchange commit

--- app/src/main/java/com/finance/portfollio/AsyncTasks/ExchangeRatesRetriever.java
+++ app/src/main/java/com/finance/portfollio/AsyncTasks/ExchangeRatesRetriever.java
@@ -1,38 +1,105 @@
 package com.finance.portfollio.AsyncTasks;
 
-
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
-import org.json.JSONArray;
+import com.finance.portfollio.utils.GlobalVariables;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-// Exchange rates API: https://github.com/exchangeratesapi/exchangeratesapi
-public class ExchangeRatesRetriever extends AsyncTask<JSONArray, Void, ArrayList> {
+public class ExchangeRatesRetriever extends AsyncTask<String, Integer, JSONObject> {
+    public static final String FOREIGN_EXCHANGE_LOG_TAG = "FOREIGN_EXCHANGE";
+    JSONObject jsonObject;
+    Context context;
 
-    public ExchangeRatesRetriever() {
+    public static final int DIALOG_FOREIGN_EXCHANGE_PROGRESS = 0;
+    private ProgressDialog progressDialog;
 
-    }
-
-    @Override
-    protected ArrayList doInBackground(JSONArray... jsonArrays) {
-
-        return null;
-    }
-
-    @Override
-    protected void onProgressUpdate(Void... values) {
-        super.onProgressUpdate(values);
+    public ExchangeRatesRetriever(Context context){
+        this.context = context;
     }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle("Foreign Exchange Rates");
+        progressDialog.setMessage("Loading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setCancelable(false);
+        progressDialog.setMax(100);
+        progressDialog.show();
     }
 
     @Override
-    protected void onPostExecute(ArrayList arrayList) {
-        super.onPostExecute(arrayList);
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        progressDialog.setProgress(values[0]);
     }
+
+    @Override
+    protected JSONObject doInBackground(String... strings) {
+        Log.e(FOREIGN_EXCHANGE_LOG_TAG, "DO IN BACKGROUND FOREIGN_EXCHANGE");
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strings[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            jsonObject = readStream(in);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+        System.out.println("Foreign Exchange Rates:");
+        System.out.println(jsonObject.toString());
+        return jsonObject;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject) {
+        super.onPostExecute(jsonObject);
+        Log.e(FOREIGN_EXCHANGE_LOG_TAG, "ON POST EXECUTE FOREIGN_EXCHANGE");
+        JSONObject jsonRates = null;
+        try {
+            jsonRates = jsonObject.getJSONObject("rates");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        for(int i = 0; i < GlobalVariables.CountryCodes.length; i++) {
+            try {
+                GlobalVariables.CountryRates[i] = jsonRates.getDouble(GlobalVariables.CountryCodes[i]);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        progressDialog.dismiss();
+    }
+
+    private JSONObject readStream(InputStream is) throws JSONException {
+        JSONObject jsonObject;
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            jsonObject = new JSONObject(bo.toString());
+            return jsonObject;
+        } catch (Exception e) {
+            return new JSONObject("Error");
+        }
+    }
+
+
 }
--- app/src/main/java/com/finance/portfollio/ForeignExchangeActivity.java
+++ app/src/main/java/com/finance/portfollio/ForeignExchangeActivity.java
@@ -3,12 +3,68 @@ package com.finance.portfollio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.finance.portfollio.AsyncTasks.ExchangeRatesRetriever;
+import com.finance.portfollio.utils.AsyncTaskHelper;
+import com.finance.portfollio.utils.CommonUtils;
+import com.finance.portfollio.utils.GlobalVariables;
+
+import java.util.Arrays;
 
 public class ForeignExchangeActivity extends AppCompatActivity {
+    TextView textViewRates;
+    Spinner spinnerCountries;
+    ArrayAdapter arrayAdapterCountries;
+
+    private void InitializeComponents()
+    {
+        textViewRates = findViewById(R.id.rates);
+        spinnerCountries = findViewById(R.id.country_spinner);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foreign_exchange);
+        InitializeComponents();
+        HandleSpinners();
+        new ExchangeRatesRetriever(this).execute("https://api.exchangeratesapi.io/latest");
+
     }
+
+    private void HandleSpinners() {
+        HandleSpinnerCountries();
+    }
+
+    private void HandleSpinnerCountries()
+    {
+        arrayAdapterCountries = new ArrayAdapter(this, android.R.layout.simple_spinner_item, CommonUtils.GetSpinnerStringArrayWithHeaderTitle(spinnerCountries, Arrays.asList(GlobalVariables.CountryCodes), ""));
+        spinnerCountries.setAdapter(arrayAdapterCountries);
+        spinnerCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0)
+                {
+                    textViewRates.setText(R.string.message_display_foreign_rates);
+                }
+                else
+                {
+                    String rate = String.valueOf(GlobalVariables.CountryRates[i-1]);
+                    rate = rate + " " + GlobalVariables.CountryCodes[i-1];
+                    textViewRates.setText(rate);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
 }
